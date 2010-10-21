78e103cacbde160c12463ba8af061d53e762733e
mathieuancelin <mathieu.ancelin@gmail.com>
2010-10-21 17:58:58 +0200

    Make GUI singleton. Enable CDI with beans.xml

--- osgi-cdi/src/main/java/com/serli/osgi/cdi/gui/SpellCheckerGui.java
+++ osgi-cdi/src/main/java/com/serli/osgi/cdi/gui/SpellCheckerGui.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
