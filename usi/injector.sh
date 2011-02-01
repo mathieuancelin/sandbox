027594009f75d1ba1337b7082025759dfd4b1ad4
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-02-01 16:54:53 +0100

    Change some things

--- usi/injector.sh
+++ usi/injector.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..5000}
+for i in {1..1000}
 do
     curl --silent -d "{ firstname:'maurice$i', lastname:'plutanfiard$i', mail:'maurice$i.plutanfiard@gmail.com', password:'xxxxx' }" http://localhost:8080/api/user
     #echo call $i 
